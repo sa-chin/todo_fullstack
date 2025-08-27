c687a54ca22bcbb892650cea28ca194cab5fc827
Sachin Mangalick <srmangalick@gmail.com>
2025-08-27 19:47:32 -0400

    final updates

--- app.py
+++ app.py
@@ -8,5 +8,29 @@ tasks = ['Buy groceries', 'Complete coding tutorial', 'Walk the dog']
 def index():
     return render_template('index.html')
 
+@app.route('/add', methods = ['POST'])
+def add_task():
+    new_task = request.form.get('newTask')
+    if new_task:
+        tasks.append(new_task)
+    return redirect(url_for('index'))
+
+@app.route('/complete', methods = ['POST'])
+def complete_tasks():
+    completed_tasks = request.form.getlist('taskCheckbox')
+    for index in map(int, completed_tasks):
+        if 1 <= index <= len(tasks):
+            tasks[index - 1] += " - Completed"
+    return redirect(url_for('index'))
+
+@app.route('/delete', methods = ['POST'])
+def delete_tasks():
+    tasks_to_delete = request.form.getlist('taskCheckbox')
+    tasks_to_delete.sort(reverse = True)
+    for index in map(int, tasks_to_delete):
+        if 1 <= index <= len(tasks):
+            del tasks[index - 1]
+    return redirect(url_for('index'))
+
 if __name__ == '__main__':
     app.run(debug = True)
